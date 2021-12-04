96b87c6875af2d0d3e9c250777f03f95aaeb3abd
ArjDev69 <bruno96araujo@gmail.com>
2021-12-04 20:06:09 -0300

    feat: added utils

---
+++ src/utils/Constants.ts
@@ -0,0 +1,10 @@
+export const appLabels = {
+  appName: "MyArt's",
+};
+
+export const URL = {
+  auth: 'https://www.abibliadigital.com.br/api/',
+  arts: 'https://api.artic.edu/api/v1/artworks',
+  imageStart: 'https://www.artic.edu/iiif/2/{',
+  imageEnd: '}/full/843,/0/default.jpg',
+};
---
+++ src/utils/index.ts
@@ -0,0 +1,6 @@
+export const isValidEmail = (email: string) => {
+  const emailRegex = RegExp(
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+  );
+  return emailRegex.test(email);
+};
